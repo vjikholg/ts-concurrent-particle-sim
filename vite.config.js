a071d8a6e13719fc0da8c846a90e8369d6eb0ec7
vjikholg <yuvict1@outlook.com>
2026-02-06 12:33:37 -0800

    test deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 export default { 
     plugins: [
         crossOriginIsolation(),
-    ]
+    ],
+    base: '/ts-concurrent-particle-sim'
 }
